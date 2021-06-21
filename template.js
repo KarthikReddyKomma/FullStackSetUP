37d237146e5b756f37bd3f184c9486c1afd90567
Karthik Reddy <karthik.reddy@tallysolutions.com>
2021-06-21 10:29:18 +0530

    Mern Setup

---
+++ template.js
@@ -0,0 +1,15 @@
+
+export default () => {
+    return `<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="urf-8">
+            <title> Mern tutorial</title>
+        </head>
+    
+        <body>
+            <div id = "root"></div>
+            <script type="text/javascript" src="/dist/bundle.js"></script>
+        </body>
+    </html>`
+}
